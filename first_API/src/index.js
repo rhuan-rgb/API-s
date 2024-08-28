cddb6397d85fcc6a9d5a1d47da474fb2b02152f3
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2024-08-28 13:38:26 -0300

    atividade autenticação de email e senha

--- first_API/src/index.js
+++ first_API/src/index.js
@@ -17,6 +17,37 @@ class AppController {
 
   routes() {
     // Define uma rota GET para o caminho /health/
+    const users = [];
+
+    this.express.post("/users", (req, res) => {
+      const {id, nome, email, senha} = req.body;
+      users.push({id,nome,email,senha});
+      res.status(200).send({message:'usuário cadastrado com sucesso'});
+    });
+
+    this.express.post("/auth", (req, res) => {
+      const {email, senha} = req.body;
+      const user = users.find((user) => user.email == email && user.senha == senha);
+      if(user){
+        res.status(200).send({message: "autenticação aprovada. Usuário logado."})
+      }
+      else{
+        res.status(400).send({message: "autenticação reprovada. Credenciais faltando ou incorretas."})
+      }
+    })
+
+
+    this.express.get("/users/:id", (req, res) => {
+      const {id} = req.params;
+      const user = users.find((user) => user.id == id);
+      if(user){
+        res.status(200).send(user);
+      }
+      else{
+        res.status(400).send({message: "Usuário não encontrado"});
+      }
+    });
+
     this.express.get("/health/", (req, res) => {
       res.send({ status: "OK", nome: "Rhuan" });
     });//essa rota é usada para verificar se a API está ok
